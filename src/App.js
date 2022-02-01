29e75810c1d6d4f864191f2bcc40dc3b0b9d931b
matty-serwer <matthew.serwer@gmail.com>
2022-02-01 17:12:13 -0500

    Setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
-    <div className="App" />
-
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// styles
+import './Landing.css';
+
+function Landing() {
+    return (
+        <div className="landing-page">
+            
+            <div className="landing-header-container">
+                <div className="landing-header">
+                        <h1 className="landing-h1">Matthew Serwer</h1>
+                        <h2 className="landing-h2">The Musings Of A Mad Developer</h2>
+                        <h3 className="landing-h3">Featuring projects by Matthew Serwer</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
